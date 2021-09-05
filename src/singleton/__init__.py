8178bc7ba4cfa39ddf9670da5c1bd275779bce4a
Sami Abdul-Wahid <syntapy@pm.me>
2021-09-05 15:15:51 -0700

    Nested class attempt, but won't work

--- src/singleton/__init__.py
+++ src/singleton/__init__.py
@@ -1,8 +1,16 @@
+import functools
+make_key = functools._make_key
+
 __version__ = '1.0.0'
 
-class Singleton(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+class MSingleton:
+    __classes = {}
+    def __new__(mcls, *args, **kwargs):
+        class Singleton(type):
+            _instances = {}
+            def __call__(cls, *args, **kwargs):
+                hashval = str(make_key(args, kwargs, True))
+                if hashval not in cls._instances:
+                    cls._instances[hashval] = super(Singleton, cls).__call__(*args, **kwargs)
+                return cls._instances[hashval]
+        return Singleton
